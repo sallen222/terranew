e2c6a0fd1fffa136308a160881089d8335e3fe95
sallen222 <222sallen@gmail.com>
2022-12-19 18:37:33 -0500

    Added .gitignore builder function

--- main.go
+++ main.go
@@ -7,8 +7,7 @@ import (
 	"os/exec"
 )
 
-func main () {
-	
+func main () {	
 	if !terraformExists() {
 		log.Fatal("Terraform is not installed. Please install Terraform before running this program.")
 	}
@@ -20,6 +19,11 @@ func main () {
 	
 	runInit()
 	makeFiles()
+	if isGitRepo() {
+		gitignore()
+	} else {
+		log.Println("Not a git repository. Skipping .gitignore creation.")
+	}
 }
 
 func terraformExists() bool {
@@ -78,20 +82,50 @@ func makeFiles() {
 		log.Fatal(err)
 	}
 	defer tfvars.Close()
+}
 
-	if isGitRepo() {
-		_, err = os.Stat(".gitignore")
-		if err != nil {
-			if os.IsNotExist(err) {
-				ignore, err := os.Create(".gitignore")
-				if err != nil {
-					log.Fatal(err)
-				}
-				defer ignore.Close()
-				ignore.WriteString("*.tfvars\n")
-			} else {
-				os.WriteFile(".gitignore", []byte("*.tfvars\n"), 0777)
+func gitignore () {
+	_, err := os.Stat(".gitignore")
+	if err != nil {
+		if os.IsNotExist(err) {
+			ignore, err := os.Create(".gitignore")
+			if err != nil {
+				log.Fatal(err)
 			}
+			defer ignore.Close()
+
 		}
 	}
+	
+
+	var gi [15][]byte
+	gi[0]=[]byte("**/.terraform/*\n")
+	gi[1]=[]byte("*.tfstate\n")
+	gi[2]=[]byte("*.tfstate.*\n")
+	gi[3]=[]byte("crash.log\n")
+	gi[4]=[]byte("crash.*.log\n")
+	gi[5]=[]byte("*.tfvars\n")
+	gi[6]=[]byte("*.tfvars.json\n")
+	gi[7]=[]byte("override.tf\n")
+	gi[8]=[]byte("override.tf.json\n")
+	gi[9]=[]byte("*_override.tf\n")
+	gi[10]=[]byte("*_override.tf.json\n")
+	gi[11]=[]byte(".terraformrc\n")
+	gi[12]=[]byte("terraform.rc\n")
+	gi[13]=[]byte(".terraform.lock.hcl\n")
+	gi[14]=[]byte("*.exe\n")
+
+	f, err := os.OpenFile(".gitignore", os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, item := range gi {
+		_, err := f.Write(item)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	defer f.Close()
 }
